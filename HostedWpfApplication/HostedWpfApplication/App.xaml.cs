3013f8347ad39c194f646c4497b8edb34d3ddc53
drjofu <drjofu@web.de>
2020-04-30 16:59:00 +0200

    Init Hosted WPF

---
+++ HostedWpfApplication/HostedWpfApplication/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace HostedWpfApplication
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    public static IHost AppHost;
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+      // Host konfigurieren
+      AppHost = Host.CreateDefaultBuilder(e.Args)
+       .ConfigureServices(ConfigureServices)
+       .Build();
+
+      // Host starten
+      await AppHost.StartAsync();
+
+      // Hauptfenster einrichten
+      var mainWindow =new MainWindow();
+
+      // ViewModel aus DI-Container laden
+      mainWindow.DataContext = AppHost.Services.GetService<MainViewModel>();
+      mainWindow.Show();
+    }
+
+    private void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+    {
+      // Hier werden die Services für Dependency Injection bereitgestellt
+      services.AddSingleton<MainViewModel>();
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+      // Beim Beenden Host stoppen und alles aufräumen
+      using (AppHost) await AppHost.StopAsync();
+    }
+  }
+}
---
+++ HostedWpfApplication/HostedWpfApplication/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace HostedWpfApplication
+{
+  public class MainViewModel : INotifyPropertyChanged
+  {
+    private readonly IConfiguration configuration;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName]string propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public string Title { get; set; }
+
+    public MainViewModel(IConfiguration configuration)
+    {
+      this.configuration = configuration;
+
+      this.Title = configuration["title"] ?? "nicht konfiguriert";
+    }
+
+  }
+}
