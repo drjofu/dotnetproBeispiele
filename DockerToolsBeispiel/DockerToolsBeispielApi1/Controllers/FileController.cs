d94191beb7bc172589785c82f333e79718838a4f
drjofu <drjofu@web.de>
2020-11-19 20:08:56 +0100

    Docker-Beispiel

---
+++ DockerToolsBeispiel/DockerToolsBeispielApi1/Controllers/FileController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using io = System.IO;
+
+namespace DockerToolsBeispielApi1.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class FileController : ControllerBase
+  {
+    private const string path = "/var/lib/data/";
+
+    [HttpGet]
+    public async Task<string> Get(string filename)
+    {
+      Console.WriteLine($"Datei {filename} abgerufen");
+      return await io.File.ReadAllTextAsync($"{path}{filename}.txt");
+    }
+
+    [HttpPost]
+    public async Task Post(string filename, [FromBody] string content)
+    {
+      if (!io.Directory.Exists(path))
+        io.Directory.CreateDirectory(path);
+
+      await io.File.WriteAllTextAsync($"{path}{filename}.txt", content);
+      var d = Environment.CurrentDirectory;
+    }
+  }
+}
---
+++ DockerToolsBeispiel/DockerToolsBeispielApi1/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerToolsBeispielApi1.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class UserController : ControllerBase
+  {
+    private readonly IConfiguration configuration;
+    private readonly ILogger<UserController> logger;
+
+    public UserController(IConfiguration configuration, ILogger<UserController> logger)
+    {
+      this.configuration = configuration;
+      this.logger = logger;
+    }
+
+    [HttpGet]
+    public UserInfo Get()
+    {
+      logger.LogInformation("Benutzerdaten abgerufen");
+
+      return new UserInfo(configuration["user"], configuration["password"]);
+    }
+  }
+
+  public record UserInfo(string User,string Password);
+}
