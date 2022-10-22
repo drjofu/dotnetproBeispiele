4ec04bf145035bbedadf438f0c056ba82bb5c926
drjofu <drjofu@web.de>
2022-10-22 19:57:04 +0200

    Channels

---
+++ QueueProcessingWithChannels/QueueProcessingWithChannels/Auftragseingang.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueProcessingWithChannels
+{
+  public record Artikel(string Bezeichnung, int Verpackungszeit);
+  public record Adresse(string Ort, int Versandzeit);
+  public record Bestellung(int Auftragsnummer, IEnumerable<Artikel> Artikel, Adresse Adresse);
+
+  public class Auftragseingang
+  {
+    private Artikel[] artikelliste = new Artikel[] {
+      new Artikel("Hemd",2),
+      new Artikel("Geschirr",5),
+      new Artikel("Bild",3),
+      new Artikel("Ball",1),
+      new Artikel("Fernseher",4),
+      new Artikel("Spiegel",4)
+    };
+
+    private Adresse[] adressen = new Adresse[]
+    {
+      new Adresse("Köln",1),
+      new Adresse("München",1),
+      new Adresse("Wien",2),
+      new Adresse("Brüssel",2),
+      new Adresse("London",3),
+      new Adresse("Wellington",5),
+      new Adresse("Tokyo",3),
+      new Adresse("Ushuaia",5),
+      new Adresse("Stanley",8),
+      new Adresse("Edinburgh of the Seven Seas",30)
+    };
+
+    public async IAsyncEnumerable<Bestellung> GetBestellungen(int anzahl)
+    {
+      int auftragsnummer = 1;
+
+      while (anzahl > 1)
+      {
+        anzahl--;
+        auftragsnummer++;
+        await Task.Delay(300);
+
+        var n = Random.Shared.Next(1, 10);
+        var artikel = new Artikel[n];
+        for (int i = 0; i < n; i++) artikel[i] = artikelliste[Random.Shared.Next(artikelliste.Length)];
+        var bestellung = new Bestellung(auftragsnummer, artikel, adressen[Random.Shared.Next(adressen.Length)]);
+        yield return bestellung;
+      }
+    }
+  }
+}
---
+++ QueueProcessingWithChannels/QueueProcessingWithChannels/Program.cs
@@ -0,0 +1,142 @@
+﻿using QueueProcessingWithChannels;
+using System.Diagnostics;
+using System.Threading.Channels;
+
+public class Program
+{
+
+  static Stopwatch stopwatch = Stopwatch.StartNew();
+  static Channel<Bestellung> channelAuftragseingang = Channel.CreateUnbounded<Bestellung>();
+  static Channel<Bestellung> channelVersandInland = Channel.CreateBounded<Bestellung>(new BoundedChannelOptions(5) { SingleReader = true });
+  static Channel<Bestellung> channelVersandAusland = Channel.CreateBounded<Bestellung>(2);
+
+  static Auftragseingang auftragseingang = new Auftragseingang();
+  static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+  static CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+  public static async Task Main()
+  {
+    AbbruchDurchBenutzerÜberwachen();
+
+    var tasks = new List<Task>();
+
+    tasks.Add(BestellungenVerarbeiten());
+
+    for (int i = 0; i < 3; i++)
+    {
+      tasks.Add(VerpackungsteamStarten());
+    }
+
+    tasks.Add(PaketversandAusführen(channelVersandInland));
+    tasks.Add(PaketversandAusführen(channelVersandAusland));
+
+    await Task.WhenAll(tasks);
+
+    if (cancellationToken.IsCancellationRequested)
+      Print("Vorzeitiger Abbruch", ConsoleColor.Red);
+    else
+      Print("Alles erledigt", ConsoleColor.Green);
+
+
+  }
+
+  private static async Task PaketversandAusführen(Channel<Bestellung> channelVersand)
+  {
+    try
+    {
+      Print($"Logistiker beginnt mit dem Versenden der Pakete", ConsoleColor.Cyan);
+      await foreach (var item in channelVersand.Reader.ReadAllAsync(cancellationToken))
+      {
+        Print($"Versand für {item.Auftragsnummer} begonnen", ConsoleColor.Cyan);
+        await Task.Delay(item.Adresse.Versandzeit * 1000);
+        Print($"Bestellung {item.Auftragsnummer} in {item.Adresse.Ort} ausgeliefert", ConsoleColor.Cyan);
+      }
+      Print($"Logistiker hat alle Versandaufträge abgeschlossen", ConsoleColor.Cyan);
+    }
+    catch (OperationCanceledException)
+    {
+      Print("Logistiker hat Arbeit abgebrochen", ConsoleColor.Red);
+    }
+  }
+
+  private static void AbbruchDurchBenutzerÜberwachen()
+  {
+    Task.Run(() =>
+    {
+
+      Console.WriteLine("Zum Beenden Eingabetaste drücken");
+      Console.ReadLine();
+      Console.WriteLine("Abbruch aller Vorgänge");
+      cancellationTokenSource.Cancel();
+    });
+  }
+
+  static async Task VerpackungsteamStarten()
+  {
+    try
+    {
+      await foreach (var bestellung in channelAuftragseingang.Reader.ReadAllAsync(cancellationToken))
+      {
+        await Verpacken(bestellung);
+      }
+
+      Print("Verpackungsteam ist fertig", ConsoleColor.Green);
+
+    }
+    catch (OperationCanceledException)
+    {
+      Print("Verpackungsteam hat Arbeit abgebrochen", ConsoleColor.Red);
+    }
+  }
+
+  static async Task BestellungenVerarbeiten()
+  {
+    await foreach (var bestellung in auftragseingang.GetBestellungen(30))
+    {
+      Print($"Bestellung {bestellung.Auftragsnummer} mit {bestellung.Artikel.Count()} Artikeln nach {bestellung.Adresse.Ort}", ConsoleColor.Yellow);
+
+      try
+      {
+        await channelAuftragseingang.Writer.WriteAsync(bestellung, cancellationToken);
+
+      }
+      catch (TaskCanceledException)
+      {
+        Print("BestellungenVerarbeiten abgebrochen", ConsoleColor.Red);
+      }
+    }
+
+    channelAuftragseingang.Writer.Complete();
+  }
+
+  static async Task Verpacken(Bestellung bestellung)
+  {
+    Print($"Verpackung {bestellung.Auftragsnummer} begonnen", ConsoleColor.Magenta);
+    await Task.Delay(bestellung.Artikel.Sum(a => a.Verpackungszeit) * 100);
+    Print($"Verpackung {bestellung.Auftragsnummer} beendet, jetzt versenden", ConsoleColor.Magenta);
+    if (bestellung.Adresse.Versandzeit > 1)
+    {
+      await channelVersandAusland.Writer.WriteAsync(bestellung, cancellationToken);
+      Print($"Auftrag {bestellung.Auftragsnummer} ins Ausland versendet", ConsoleColor.Blue);
+    }
+    else
+    {
+      await channelVersandInland.Writer.WriteAsync(bestellung, cancellationToken);
+      Print($"Auftrag {bestellung.Auftragsnummer} ins Inland versendet", ConsoleColor.Blue);
+    }
+  }
+
+  static object syncObj = new();
+  static void Print(string text, ConsoleColor color)
+  {
+    lock (syncObj)
+    {
+      var previousColor = Console.ForegroundColor;
+      Console.ForegroundColor = color;
+      Console.WriteLine($"{stopwatch.ElapsedMilliseconds:00,000} {text}");
+      Console.ForegroundColor = previousColor;
+
+    }
+  }
+
+}
