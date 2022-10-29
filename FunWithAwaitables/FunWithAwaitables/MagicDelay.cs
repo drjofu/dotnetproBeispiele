2d086b33d1ae7b657d78a623259756630c1ad72d
drjofu <drjofu@web.de>
2022-10-29 14:29:01 +0200

    AwaiterMagic

---
+++ FunWithAwaitables/FunWithAwaitables/MagicDelay.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithAwaitables
+{
+  public class MagicDelay
+  {
+    private readonly TimeSpan timeSpan;
+    public TimeSpan TimeSpan => timeSpan;
+
+    private MagicDelay(TimeSpan timeSpan)
+    {
+      this.timeSpan = timeSpan;
+    }
+
+    public static MagicDelay Milliseconds(int milliseconds)
+    {
+      return new MagicDelay(TimeSpan.FromMilliseconds(milliseconds));
+    }
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.runtime.compilerservices.taskawaiter?f1url=%3FappId%3DDev16IDEF1%26l%3DEN-US%26k%3Dk(System.Runtime.CompilerServices.TaskAwaiter)%3Bk(DevLang-csharp)%26rd%3Dtrue&view=net-6.0
+    // TaskAwaiter: This type is intended for compiler use only.
+
+    public TaskAwaiter GetAwaiter()
+    {
+      return Task.Delay(timeSpan).GetAwaiter();
+    }
+  }
+
+
+  public static class MagicExtensions
+  {
+    public static TaskAwaiter GetAwaiter(this MagicDelay magicDelay)
+    {
+      return Task.Delay(magicDelay.TimeSpan).GetAwaiter();
+    }
+
+    public static TaskAwaiter GetAwaiter(this TimeSpan timeSpan)
+    {
+      return Task.Delay(timeSpan).GetAwaiter();
+    }
+
+    public static TaskAwaiter GetAwaiter(this int milliseconds)
+    {
+      return Task.Delay(TimeSpan.FromMilliseconds(milliseconds)).GetAwaiter();
+    }
+
+
+  }
+}
---
+++ FunWithAwaitables/FunWithAwaitables/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//await 1000;
+
+using FunWithAwaitables;
+using System.Diagnostics;
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+await MagicDelay.Milliseconds(1000);
+
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+await TimeSpan.FromMilliseconds(2000);
+
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+await 1000;
+
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
