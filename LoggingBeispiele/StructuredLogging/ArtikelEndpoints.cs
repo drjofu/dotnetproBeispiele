f11bf23348042746475f6fbfa68b610953ad5565
drjofu <drjofu@web.de>
2023-12-06 12:50:34 +0100

    seq

--- LoggingBeispiele/StructuredLogging/ArtikelEndpoints.cs
+++ LoggingBeispiele/StructuredLogging/ArtikelEndpoints.cs
@@ -1,12 +1,16 @@
 ﻿
+
 namespace StructuredLogging
 {
   public static partial class ArtikelEndpoints
-  {public  record Info(string Infotext="eine Info");
-    [LoggerMessage(Message = "Bearbeitung Artikel Nr. {artikelnummer}", EventId = 123)]
-    public static partial void LogArtikelV3Precompiled(this ILogger logger, int artikelnummer, [LogProperties] Artikel artikel,[LogProperties]Info info, LogLevel logLevel = LogLevel.Warning);
+  {
+    public record Info(string Infotext = "eine Info");
 
-    public static void LogArtikelV3Precompiled(this ILogger logger, Artikel artikel, LogLevel logLevel = LogLevel.Warning) => LogArtikelV3Precompiled(logger, artikel.Nummer, artikel,new Info(), logLevel);
+    [LoggerMessage(Message = "Bearbeitung Artikel Nr. {artikelnummer}", EventId = 123)]
+    public static partial void LogArtikelV3Precompiled(this ILogger logger, int artikelnummer, [LogProperties] Artikel artikel, [LogProperties] Info info, LogLevel logLevel = LogLevel.Warning);
+
+    public static void LogArtikelV3Precompiled(this ILogger logger, Artikel artikel, LogLevel logLevel = LogLevel.Warning) =>
+      LogArtikelV3Precompiled(logger, artikel.Nummer, artikel, new Info(), logLevel);
 
     [LoggerMessage(Message = "Bearbeitung Artikel {artikel}", EventId = 123)]
     public static partial void LogArtikelV4Precompiled(this ILogger logger, Artikel artikel, LogLevel logLevel = LogLevel.Warning);
@@ -17,6 +21,8 @@ namespace StructuredLogging
     [LoggerMessage("Logging a list", Level = LogLevel.Critical)]
     public static partial void LogList(this ILogger logger, IEnumerable<string> myList);
 
+    [LoggerMessage("Logging an exception", Level = LogLevel.Critical)]
+    public static partial void LogException(this ILogger logger, Exception exception, [LogProperties] Artikel artikel);
 
     public static WebApplication MapArtikelEndpoints(this WebApplication app)
     {
@@ -29,8 +35,28 @@ namespace StructuredLogging
       group.MapGet("v4", GetArtikelV4);
       group.MapGet("errorlist", GetWithErrorlist);
       group.MapGet("keyvaluelist", GetWithKeyValuelist);
+      group.MapGet("exception", GetWithException);
+
       return app;
     }
+
+    private static IResult GetWithException(ILoggerFactory loggerFactory)
+    {
+      var logger = loggerFactory.CreateLogger(nameof(ArtikelEndpoints));
+      var artikel = new Artikel { Bezeichnung = "Kaputte Uhr", Nummer = 77777, Preis = 13.13, Herstellerhinweise = new() { "sehr schöne Standuhr", "zeigt 2x täglich die richtige Zeit an" } };
+
+      try
+      {
+        throw new ApplicationException("damit war zu rechnen...", new InvalidOperationException("so geht das nicht..."));
+      }
+      catch (Exception ex)
+      {
+        logger.LogException(ex, artikel);
+      }
+
+      return Results.Problem("das ist ein Problem");
+    }
+
     private static string GetWithKeyValuelist(ILoggerFactory loggerFactory)
     {
       var logger = loggerFactory.CreateLogger(nameof(ArtikelEndpoints));
